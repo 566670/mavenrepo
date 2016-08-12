cf3f54cdff1cfe91a275b7b49c538fb0a23ef3c9
koti <kkr.kothapally@gmail.com>
2016-08-12 14:23:18 +0530

    cvommit  1

---
+++ MavenApp/Mavenn/src/main/java/com/kk/kk/Samplee.java
@@ -0,0 +1,15 @@
+package com.kk.kk;
+
+public class Samplee {
+	public void m1()
+	{
+		System.out.println("SDFS");
+	}
+	public void m2()
+	{
+		System.out.println("asdfa");
+		System.out.println("sdgfsd");
+		System.out.println("ada");
+	}
+
+}
---
+++ mamam/src/main/java/com/ngfg/hh/haiii.java
@@ -0,0 +1,9 @@
+package com.ngfg.hh;
+
+public class haiii {
+
+	public void m1()
+	{
+		System.out.println("asda");
+	}
+}
